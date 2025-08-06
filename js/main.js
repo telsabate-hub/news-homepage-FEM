3793597079f8aea67b7470c588222c71385b3d81
Tel <telsabate.business@gmail.com>
2025-08-06 20:03:47 +0800

    Fixed main window overlay when menu opens. Fixed styles for new articles section and for main news area.

--- js/main.js
+++ js/main.js
@@ -5,6 +5,7 @@ menuBtn.addEventListener( "click", function(e){
     const closeIcon = menuBtn.querySelector('.close-icon');
     const mainNav = document.getElementById('mainNav');
     const isExpanded = menuBtn.getAttribute('aria-expanded') === 'true' || false;
+    const menuOverlay = document.getElementById('menuOverlay');
   
     menuBtn.setAttribute('aria-expanded', !isExpanded);
     mainNav.hidden = isExpanded;
@@ -13,4 +14,12 @@ menuBtn.addEventListener( "click", function(e){
     // Toggle icon visibility
     openIcon.hidden = !isExpanded;
     closeIcon.hidden = isExpanded;
+
+    if( !isExpanded ){  //Menu is about to be shown.
+        menuOverlay.style.display = "block";
+    } else {
+        menuOverlay.style.display = "none";
+    }
+    
+    document.body.classList.toggle( "menu-open" );
 });
